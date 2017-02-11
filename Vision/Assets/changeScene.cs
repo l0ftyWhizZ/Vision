e239dbff959ce6072d1c4dda4e0322b52542be8b
ankit5000 <ankit5000@ymail.com>
2017-02-11 15:26:04 +0530

    change Scene

--- Vision/Assets/changeScene.cs
+++ Vision/Assets/changeScene.cs
@@ -1,16 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class changeScene : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public void startTheShow(){
+
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
 }
